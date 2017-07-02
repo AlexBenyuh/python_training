8492e25da1a34a0e5f9d96aeeebdaaf38794ab9b
AlexBenyuh <alexbenyuh@gmail.com>
2017-07-02 13:59:43 +0300

    added intelect fixture

--- conftest.py
+++ conftest.py
@@ -1,9 +1,24 @@
 import pytest
 from fixture.application import Application
 
-@pytest.fixture(scope = "session")
+fixture = None
+
+
+@pytest.fixture
 def app(request):
-    fixture = Application()
-    fixture.open_home_page()
-    request.addfinalizer(fixture.destroy)
+    global fixture
+    if fixture is None:
+       fixture = Application()
+       fixture.open_home_page()
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.open_home_page()
+    return fixture
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.destroy()
+    request.addfinalizer(fin)
     return fixture
