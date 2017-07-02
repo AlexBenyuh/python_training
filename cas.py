bd132e4e8b16191b7802d91cd64d4fd8f1b13e45
AlexBenyuh <alexbenyuh@gmail.com>
2017-07-02 11:26:44 +0300

    added fixture and logout method
    changed pytest instead of unittets

--- cas.py
+++ cas.py
@@ -1,46 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.chrome.webdriver import WebDriver
-import unittest
+
+import pytest
 from credentials import Credentials
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class cas2(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+def test_cas2(app):
+    app.login(Credentials(login="380961451058", password="MK2prod_2016"))
+    app.hello_mk()
+    app.logout()
 
-    def hello_mk(self, wd):
-        wd.find_element_by_css_selector("a.header-bar__logo-img").click()
-
-    def login(self, wd, credentials):
-        self.open_home_page(wd)
-        wd.find_element_by_css_selector("input.form-control").click()
-        wd.find_element_by_css_selector("input.form-control").clear()
-        wd.find_element_by_css_selector("input.form-control").send_keys(credentials.login)
-        wd.find_element_by_css_selector("button.btn.btn-default").click()
-        wd.find_element_by_css_selector("input.form-control").click()
-        wd.find_element_by_css_selector("input.form-control").send_keys(credentials.password)
-        wd.find_element_by_xpath("//section[@id='section-left']/section[2]/div/div[1]").click()
-        wd.find_element_by_css_selector("button.btn.btn-default").click()
-
-    def open_home_page(self, wd):
-        wd.get("https://new.kyivstar.ua/ecare/")
-
-    def test_cas2(self):
-        success = True
-        wd = self.wd
-        self.login(wd, Credentials(login="380961451058", password="MK2prod_2016"))
-        self.hello_mk(wd)
-        self.assertTrue(success)
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
