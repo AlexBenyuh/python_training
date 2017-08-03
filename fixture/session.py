e0c7a902deb0f9303699efb458857d998779e559
Alex <alexbenyuh@gmail.com>
2017-08-03 21:33:59 +0300

    added wait_click should be compleated
    merged wait and click selenium command for css

--- fixture/session.py
+++ fixture/session.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class SessionHelper:
 
@@ -7,15 +11,20 @@ class SessionHelper:
 
     def login(self, credentials):
         wd = self.app.wd
+        wd.wait = WebDriverWait(wd, 10)
         # self.app.open_home_page()
         wd.find_element_by_css_selector("input.form-control").click()
         wd.find_element_by_css_selector("input.form-control").clear()
         wd.find_element_by_css_selector("input.form-control").send_keys(credentials.login)
-        wd.find_element_by_css_selector("button.btn.btn-default").click()
+        self.wait_click(wd, css="button.btn.btn-default")
         wd.find_element_by_css_selector("input.form-control").click()
         wd.find_element_by_css_selector("input.form-control").send_keys(credentials.password)
         wd.find_element_by_xpath("//section[@id='section-left']/section[2]/div/div[1]").click()
         wd.find_element_by_css_selector("button.btn.btn-default").click()
+
+    def wait_click(self, wd, css):
+        wd.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, css)))
+        wd.find_element_by_css_selector(css).click()
 
     def logout(self):
         wd = self.app.wd
