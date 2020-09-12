fed2c914de2623a740e541129ae54d7e35228fb1
AlexBenyuh <alexbenyuh@gmail.com>
2020-09-12 18:03:43 +0300

    Update test_success_login.py

--- test/test_success_login.py
+++ test/test_success_login.py
@@ -3,7 +3,7 @@ from model.credentials import Credentials
 
 
 def test_success_login_mk(app):
-    app.session.login(Credentials(login="380961451058", password="MK2prod_2016"))
+    app.session.login(Credentials(login="38096000000", password="xxx"))
     app.mk.hello_mk()
     app.mk.merge_from_mk()
     app.session.logout()
